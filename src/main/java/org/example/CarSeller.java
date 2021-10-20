d7709670ffc5aaf33f60ba20eba5e84dd260f43d
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2021-10-20 20:29:00 +0300

    task fixed

--- src/main/java/org/example/CarSeller.java
+++ src/main/java/org/example/CarSeller.java
@@ -1,46 +1,55 @@
 package org.example;
 
 
-public class CarSeller {
+public class CarSeller extends Thread {
 
     private static final int THREAD_SHORT_SLEEP = 1000;
     private static final int THREAD_LONG_SLEEP = 2000;
     private static final int ZERO = 0;
+    private static final int CARS_TO_SELL = 3;
     private final CarShop carShop;
-    public CarSeller(CarShop carShop){
+
+    public CarSeller(CarShop carShop) {
         this.carShop = carShop;
     }
 
-    public void printBorder(){
+    public void printBorder() {
         System.out.println("------------------");
     }
 
-    public synchronized Car sellCar(){
+    public Car sellCar() {
         try {
-            System.out.println("Покупатель " + Thread.currentThread().getName() + " хочет купить автомобиль");
-            while (carShop.getCars().size() == ZERO){
-                System.out.println("Машин нет в наличии...\nОжидаем поставку...");
-                printBorder();
-                wait();
+            synchronized (this) {
+                System.out.println("Покупатель " + Thread.currentThread().getName() + " хочет купить автомобиль");
+                while (carShop.getCars().size() == ZERO) {
+                    System.out.println("Машин нет в наличии...");
+                    printBorder();
+                    wait();
+                }
             }
             Thread.sleep(THREAD_SHORT_SLEEP);
             System.out.println("Покупатель " + Thread.currentThread().getName() + " купил новую машину");
             printBorder();
-        }catch (InterruptedException e){
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         return carShop.getCars().remove(ZERO);
     }
 
-    public synchronized void receiveCar(){
+    public void receiveCar() {
+        int currCarVal = 0;
         try {
-            System.out.println("С завода пришла поставка, розгружаем....");
-            Thread.sleep(THREAD_LONG_SLEEP);
-            carShop.getCars().add(new Car());
-            System.out.println("Автосалон получил машину ");
-            printBorder();
-            notify();
-        }catch (Exception e){
+            while (currCarVal < CARS_TO_SELL) {
+                Thread.sleep(THREAD_LONG_SLEEP);
+                synchronized (this) {
+                    carShop.getCars().add(new Car());
+                    System.out.println("Автосалон получил машину ");
+                    printBorder();
+                    notify();
+                    currCarVal++;
+                }
+            }
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,18 +2,24 @@ package org.example;
 
 
 public class Main {
-    public static void main( String[] args ) {
+    public static void main(String[] args) throws InterruptedException {
         final CarShop carShop = new CarShop();
         int currCar = 0;
-        final int CAR_QUANTITY = 9;
-        while (currCar < CAR_QUANTITY){
-            new Thread(carShop::sellCar, "1").start();
-            new Thread(carShop::sellCar, "2").start();
-            new Thread(carShop::sellCar, "3").start();
-            new Thread(carShop::receiveCar, "factory1").start();
-            new Thread(carShop::receiveCar, "factory2").start();
-            new Thread(carShop::receiveCar, "factory3").start();
-            currCar+=3;
+        final int CAR_QUANTITY = 6;
+        final int CARS_SOLD = 3;
+        while (currCar < CAR_QUANTITY) {
+            Thread thread1 = new Thread(carShop::sellCar, "1");
+            Thread thread2 = new Thread(carShop::sellCar, "2");
+            Thread thread3 = new Thread(carShop::sellCar, "3");
+            Thread thread = new Thread(carShop::receiveCar, "factory1");
+            thread.start();
+            thread1.start();
+            thread2.start();
+            thread3.start();
+            thread1.join();
+            thread2.join();
+            thread3.join();
+            currCar += CARS_SOLD;
         }
     }
 }
