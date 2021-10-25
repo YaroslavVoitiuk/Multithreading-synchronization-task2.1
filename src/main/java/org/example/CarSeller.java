1bb03d8b67d0d1e9f0ac7bd236a329c400b7f9c9
Yaroslav Voitiuk <yaroslav.voitiuk@gmail.com>
2021-10-25 11:49:40 +0300

    task fixed 2.0

--- src/main/java/org/example/CarSeller.java
+++ src/main/java/org/example/CarSeller.java
@@ -5,7 +5,6 @@ public class CarSeller extends Thread {
 
     private static final int THREAD_SHORT_SLEEP = 1000;
     private static final int THREAD_LONG_SLEEP = 2000;
-    private static final int ZERO = 0;
     private static final int CARS_TO_SELL = 3;
     private final CarShop carShop;
 
@@ -21,19 +20,20 @@ public class CarSeller extends Thread {
         try {
             synchronized (this) {
                 System.out.println("Покупатель " + Thread.currentThread().getName() + " хочет купить автомобиль");
-                while (carShop.getCars().size() == ZERO) {
+                while (carShop.getCars().isEmpty()) {
                     System.out.println("Машин нет в наличии...");
                     printBorder();
                     wait();
                 }
+                Thread.sleep(THREAD_SHORT_SLEEP);
+                System.out.println("Покупатель " + Thread.currentThread().getName() + " купил новую машину");
+                printBorder();
             }
-            Thread.sleep(THREAD_SHORT_SLEEP);
-            System.out.println("Покупатель " + Thread.currentThread().getName() + " купил новую машину");
-            printBorder();
+
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return carShop.getCars().remove(ZERO);
+        return carShop.getCars().remove(0);
     }
 
     public void receiveCar() {
